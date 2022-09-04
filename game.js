0c1e9179d9fb33621a65313e178219536e0c293b
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-05 02:56:49 +0300

    Adding three divs for the timer (live timer, last score, best score)

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 /********************* Global Variables **********************/
 
-var elements = document.getElementsByClassName("boundary")
+var bounderies = document.getElementsByClassName("boundary")
 var playing = false
 var gamescore = 0
 var liveTimer = Date.now()
@@ -12,7 +12,25 @@ function updateHtml(){
     scoreDiv.id = "score";
     scoreDiv.style.textAlign = "center";
 
+    let stopWatch = document.getElementsByTagName("p")[1];
+    // console.log("stop watch",stopWatch);
+    // stopWatch. = document.createElement("div");
+    // stopWatch.classList.add("score_div");
+    console.log("stop watch",stopWatch);
     
+    stopWatch.innerHTML = "<div class ='stopWatch' id='live_timer'></div> <div class ='stopWatch' id='last_score'></div> <div class ='stopWatch' id='best_score'></div>";
+    stopWatch.style.display = "flex";
+    stopWatch.style.height = "120px";
+    stopWatch.style.marginTop = "30px";
+    let stopWatch_divs = document.getElementsByClassName("stopWatch");
+    for(let i=0; i < stopWatch_divs.length; i++){
+        
+        stopWatch_divs[i].style.width = '33%';
+        stopWatch_divs[i].style.height = '100%';
+        stopWatch_divs[i].style.padding = '15px';
+        stopWatch_divs[i].style.textAlign = 'center';
+    }
+    document.body.appendChild(stopWatch);
     
 
 }
