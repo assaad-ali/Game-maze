107ed12277bb4e3c6963be0c053576edc9039f74
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-05 00:26:10 +0300

    Adding global variables

---
+++ game.js
@@ -0,0 +1,7 @@
+/********************* Global Variables /**********************/
+
+var elements = document.getElementsByClassName("boundary")
+var playing = false
+var gamescore = 0
+var liveTimer = Date.now()
+
