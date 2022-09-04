cef6ce0ea4ccc1ddaa386374f45e7e07ba91e354
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-05 02:01:47 +0300

    Adding restartButton to updateHtml

--- game.js
+++ game.js
@@ -11,6 +11,13 @@ function updateHtml(){
     let scoreDiv = document.getElementsByClassName("example")[0]
     scoreDiv.id = "score"
     scoreDiv.style.textAlign = "center"
+    let restartButton = document.createElement("button")
+    restartButton.classList = "boundary"
+    restartButton.id = "restart"
+    restartButton.style = "margin: auto; text-align: center; width:100px; height:25px; display:flex; font-size: 15px; background-color: lightblue;"
+    restartButton.innerText = "Restart"
+    document.body.appendChild(restartButton)
+    
 
 }
 
