e4cd33d096c6a626c424d763785c800a2fcf06dd
Ali-Assaad <aliassaadtr@gmail.com>
2022-09-05 01:59:45 +0300

    Adding updateHtml function to add score dic

--- game.js
+++ game.js
@@ -1,7 +1,29 @@
-/********************* Global Variables /**********************/
+/********************* Global Variables **********************/
 
 var elements = document.getElementsByClassName("boundary")
 var playing = false
 var gamescore = 0
 var liveTimer = Date.now()
+var user_score = []
+
+/********************* Update Html **********************/
+function updateHtml(){
+    let scoreDiv = document.getElementsByClassName("example")[0]
+    scoreDiv.id = "score"
+    scoreDiv.style.textAlign = "center"
+
+}
+
+/********************** Initialize Game **********************/
+
+function initMaze() {
+
+    updateHtml()
+}
+
+window.onload = function(){
+
+    initMaze()
+}
+
 
